2529618cab88ef1b50a22e500dd91fd939f95eaf
PhilippePelletier <Philippe.Pelletier@outlook.com>
2025-09-23 11:54:00 -0400

    Update supabase.ts

--- web/app/lib/supabase.ts
+++ web/app/lib/supabase.ts
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 export function supaService() {
   const url = process.env.SUPABASE_URL!;
   const key = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-  return createClient(url, key);
+  return createClient(url, key, { auth: { persistSession: false } });
 }
