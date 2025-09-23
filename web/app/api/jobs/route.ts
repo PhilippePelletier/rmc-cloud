12b82ff2c9c47d14d2264346935553f82acbba09
PhilippePelletier <Philippe.Pelletier@outlook.com>
2025-09-23 11:52:35 -0400

    Update route.ts

--- web/app/api/jobs/route.ts
+++ web/app/api/jobs/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
-import { supaService } from "../lib/supabase";
+import { supaService } from "../../lib/supabase";
 
 export const dynamic = 'force-dynamic';
 
