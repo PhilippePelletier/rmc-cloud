8c8c2200dd748533b9ab923105ccc0ff99303d1d
PhilippePelletier <Philippe.Pelletier@outlook.com>
2025-09-24 10:48:56 -0400

    Update middleware.ts

--- web/middleware.ts
+++ web/middleware.ts
@@ -1,11 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createClerkClient } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  afterAuth(auth, req) {
+    if (!auth.userId && !req.nextUrl.pathname.startsWith('/sign-in')) {
+      const signInUrl = new URL('/sign-in', req.url);
+      return NextResponse.redirect(signInUrl);
+    }
+    return NextResponse.next();
+  },
+});
 
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/",
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ['/((?!_next|.*\\..*).*)'],
 };
