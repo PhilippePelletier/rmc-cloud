14d32499b6ea45eb946ef659360c7ea96919efa6
PhilippePelletier <Philippe.Pelletier@outlook.com>
2025-10-19 19:48:46 -0400

    Update layout.tsx

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -38,7 +38,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                   </>
                 )}
               </div>
-            </header>
+            <HeaderShell />
             <ToasterClient />
             {children}
           </div>
